db7adeb43754f6d98562bb54c97706e48d1e6a33
Mayra Landa <mlandae16@gmail.com>
2024-09-11 11:27:50 -0700

    add views

--- express/index.js
+++ express/index.js
@@ -1,9 +1,27 @@
+// app file
+
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.set("view engine", "ejs");
 
-const PORT = 3000;
-app.listen(PORT, () =>
-  console.log(`My first Express app - listening on port ${PORT}!`)
-);
+app.set("views", path.join(__dirname, "views"));
+
+
+app.get("/", (req, res) => {
+  res.render("index");
+})
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.use((req, res) => {
+  res.status(404).render("404", { tile: "Page Not Found" });
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+})
