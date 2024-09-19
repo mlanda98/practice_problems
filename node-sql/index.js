42d752055868732b554b4c6d719a1eb0f5e9f5a9
Mayra Landa <mlandae16@gmail.com>
2024-09-19 16:28:21 -0700

    add index.js and views

--- node-sql/index.js
+++ node-sql/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
+const usersRouter = require("./routes/usersRouter");
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send("Hello World");
-})
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use("/", usersRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
